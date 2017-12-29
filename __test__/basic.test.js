503f134ce88b960dd51006449ebc94353dfeb4a8
snobeard <fatmattmatt@gmail.com>
2017-12-29 09:58:04 -0800

    setting up heroku

---
+++ __test__/basic.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('BASIC TEST', () => {
+  test('Basic Testing to see if Travis is working', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('dotenv').config();
+
+const server = require('./lib/server');
+
+server.start();
