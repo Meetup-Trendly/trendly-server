e9621d3b9ac60f23de2e34e97cc02894c21985f0
snobeard <fatmattmatt@gmail.com>
2017-12-29 16:08:48 -0800

    connected meetup information with the profile

--- route/profile-router.js
+++ route/profile-router.js
@@ -11,20 +11,25 @@ const bearerAuthMiddleware = require('../lib/bearer-auth-middleware');
 const profileRouter = module.exports = new Router();
 
 profileRouter.post('/profiles', bearerAuthMiddleware, jsonParser, (request, response, next) => {
-
-  // superagent.get('', () => {
-
-  // })
   if(!request.account && !request.phoneNumber)
     return next(new httpErrors(404, '__ERROR__ no account, or phonenumber given'));
 
-  return new Profile ({
-    ...request.body,
-    account: request.account._id,
-  }).save()
-    .then(profile => response.json(profile))
-    .catch(next);
+  return superagent.get(`https://api.meetup.com/members/${request.body.meetupMemberId}?key=${process.env.API_KEY}&?fields=groups?%22`)
+    .then(response => {
+      return response.body;
+    })
+    .then(meetupObject => {
+      return new Profile ({
+        ...request.body,
+        meetupMemberId: meetupObject.id,
+        name: meetupObject.name,
+        account: request.account._id,
+      }).save()
+        .then(profile => response.json(profile))
+        .catch(next);
+    });
 });
+
 
 profileRouter.get('/profiles/:id', bearerAuthMiddleware, (request, response, next) => {
   if(!request.account)
