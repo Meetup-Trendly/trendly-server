aea30ced96e191b3c126b5610e6fc5147f2f8958
Dalton Carr <daltonlcarr@yahoo.com>
2018-01-02 15:53:02 -0800

    working on getting a response from twilio

--- route/sms-profile-router.js
+++ route/sms-profile-router.js
@@ -1,24 +1,49 @@
 'use strict';
 
 const { Router } = require('express');
-const jsonParser = require('body-parser').json();
+const jsonParser = require('body-parser');
 const httpErrors = require('http-errors');
 const superagent = require('superagent');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 const smsProfile = require('../model/sms-profile');
+
+const createMessage = (phoneNumber,message) => {
+  client.messages.create({
+    to: phoneNumber,
+    from: '+12065651148',
+    body: message,
+  })
+    .then(message => console.log(message.sid));
+};
+
+
 
 const log = require('../lib/logger');
 
 const smsProfileRouter = module.exports = new Router();
 
 smsProfileRouter.post('/sms-profile', jsonParser, (request, response, next) => {
+  const twiml = new MessagingResponse();
+  console.log(request.body.From);
+  // twiml.message(`Congratulations, you are all signed up \n
+  //             Here's a list of commands: _______`);
+  createMessage(request.body.From, 'Congrats you are all signed up');
+  console.log(request.body.From);
+  return;
+  response.writeHead(200, {'Content-Type': 'text/xml'});
+  response.end(twiml.toString());
+
   if(!request.body.Body || !request.body.From) {
     return next(new httpErrors(404, 'Please provide a text message and a proper phone number'));
   }
   const userInput = request.body.Body;
   const phoneNumber = request.body.From;
-  const twiml = new MessagingResponse();
+  // const twiml = new MessagingResponse();
 
   const isANumber = str => {
     return /\d/.test(str);
